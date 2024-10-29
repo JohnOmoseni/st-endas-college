c375f090d53053bc8a2f9305513c9cc5c58f3eea
John Omoseni <johnnyomoseni100@gmail.com>
2024-10-29 05:38:19 +0100

    chore: edited error page

--- src/layouts/Header2.tsx
+++ src/layouts/Header2.tsx
@@ -9,48 +9,42 @@ import { navLinks } from "@/constants/dashboard-index";
 import NavLinks from "./NavLinks";
 
 type HeaderProps = {
-	setOpenMenu?: Dispatch<SetStateAction<boolean>>;
+  setOpenMenu?: Dispatch<SetStateAction<boolean>>;
 };
 
 function Header2({ setOpenMenu }: HeaderProps) {
-	return (
-		<div className="relative z-10 flex min-h-[70px] max-sm:max-h-[80px] w-full px-3 py-1 shadow-sm">
-			<div className="row-flex-btwn mx-auto gap-4 w-full">
-				<div
-					className="icon group sm:!hidden"
-					onClick={() => setOpenMenu && setOpenMenu(true)}
-				>
-					<MenuIcon
-						className="size-6 transition-all group-hover:scale-95"
-						color="#333"
-					/>
-				</div>
+  return (
+    <div className="relative z-10 flex min-h-[70px] max-sm:max-h-[80px] w-full px-3 py-1 shadow-sm">
+      <div className="row-flex-btwn mx-auto gap-4 w-full">
+        <div className="icon group sm:!hidden" onClick={() => setOpenMenu && setOpenMenu(true)}>
+          <MenuIcon className="size-6 transition-all group-hover:scale-95" color="#333" />
+        </div>
 
-				<Link to="/dashboard" className="max-sm:w-full">
-					<LogoMobile className="block sm:hidden h-fit w-44 max-md:mx-auto " />
+        <Link to="/" className="max-sm:w-full">
+          <LogoMobile className="block sm:hidden h-fit w-44 max-md:mx-auto " />
 
-					<Logo className="hidden sm:block h-fit w-fit max-md:mx-auto " />
-				</Link>
+          <Logo className="hidden sm:block h-fit w-fit max-md:mx-auto " />
+        </Link>
 
-				<div className="lg:row-flex hidden gap-8 px-3">
-					{navLinks?.map((link, idx) => (
-						<NavLinks key={idx} {...link} />
-					))}
-				</div>
+        <div className="lg:row-flex hidden gap-8 px-3">
+          {navLinks?.map((link, idx) => (
+            <NavLinks key={idx} {...link} />
+          ))}
+        </div>
 
-				<div className={cn("sm:row-flex hidden mr-[2%]")}>
-					<Button
-						title="Portal"
-						onClick={() => {
-							toast.info("Coming soon!");
-							return;
-						}}
-						className="!min-w-[180px] rounded-lg px-5 sm:px-7"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+        <div className={cn("sm:row-flex hidden mr-[2%]")}>
+          <Button
+            title="Portal"
+            onClick={() => {
+              toast.info("Coming soon!");
+              return;
+            }}
+            className="!min-w-[180px] rounded-lg px-5 sm:px-7"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header2;
